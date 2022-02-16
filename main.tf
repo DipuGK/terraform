eb6252f536aaaa4c821e5771aaf6ad97b38c6e06
DipuGK <89657085+DipuGK@users.noreply.github.com>
2022-02-17 00:01:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {
-  name         = "virtual-machine-from-terraform"
+  name         = "virtual-machine-from-terraform1"
   machine_type = "f1-micro"
   zone         = "us-central1-a"
 
